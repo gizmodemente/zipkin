8502e63effd35446bebd1b8d0bac5ba1cf46177e
gizmodemente <gizmodemente@googlemail.com>
2017-02-05 22:51:35 +0100

    Enable a hystrix dashboard in micro-one

--- micro-one/src/main/java/es/paradigma/demo/MicroOneApplication.java
+++ micro-one/src/main/java/es/paradigma/demo/MicroOneApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 /**
  * Created by fdelosrios on 11/09/16.
@@ -11,6 +12,7 @@ import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableHystrix
+@EnableHystrixDashboard
 public class MicroOneApplication {
 
     public static void main(String[] args) {
